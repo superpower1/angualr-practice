469a107ce1c364e3fec2617d1fc4e6d23d9ce550
superpower1 <superpiglet1@gmail.com>
2017-10-20 11:04:17 +1100

    add form component

---
+++ form/src/app/my-form/my-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-form',
+  templateUrl: './my-form.component.html',
+  styleUrls: ['./my-form.component.css']
+})
+export class MyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value:any) {
+  	console.log(value);
+  }
+
+}
