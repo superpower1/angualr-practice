65729655c7d4f0e6aa5b656124bef5dffd574930
superpower1 <superpiglet1@gmail.com>
2017-11-12 23:08:12 +1100

    Modify auction_server
    add comments

--- server/build/auction_server.js
+++ server/build/auction_server.js
@@ -15,13 +15,33 @@ var Product = (function () {
     return Product;
 }());
 exports.Product = Product;
+var Comment = (function () {
+    function Comment(id, productID, timeStamp, user, rating, content) {
+        this.id = id;
+        this.productID = productID;
+        this.timeStamp = timeStamp;
+        this.user = user;
+        this.rating = rating;
+        this.content = content;
+    }
+    return Comment;
+}());
+exports.Comment = Comment;
 var products = [
     new Product(1, "M99", 1.99, 3.5, "M99 is RF", ["RF"]),
     new Product(2, "M1887", 4.99, 4.5, "M1887 is SG", ["SG"]),
     new Product(3, "G41", 6.99, 2.5, "G41 is AR", ["AR"]),
     new Product(4, "UMP45", 9.99, 5.0, "UMP45 is SMG", ["SMG"]),
     new Product(5, "M950A", 2.99, 3.0, "M950A is HG", ["HG"]),
-    new Product(6, "MG4", 7.99, 1.5, "MG4 is MG", ["MG"]),
+    new Product(6, "MG4", 7.99, 1.5, "MG4 is MG", ["MG"])
+];
+var comments = [
+    new Comment(1, 1, "2017-9-20 12:45", "sp1", 4, "Great"),
+    new Comment(2, 2, "2017-9-20 12:45", "sp1", 3, "Good"),
+    new Comment(3, 1, "2017-9-20 12:45", "sp1", 2.5, "Fair"),
+    new Comment(4, 3, "2017-9-20 12:45", "sp1", 5, "Excellent"),
+    new Comment(5, 2, "2017-9-20 12:45", "sp1", 1, "Bad"),
+    new Comment(6, 3, "2017-9-20 12:45", "sp1", 4, "Great")
 ];
 app.get('/', function (req, res) {
     res.send("Hello Express");
@@ -31,6 +51,9 @@ app.get('/api/products', function (req, res) {
 });
 app.get('/api/product/:id', function (req, res) {
     res.json(products.find(function (product) { return product.id == req.params.id; }));
+});
+app.get('/api/product/:id/comments', function (req, res) {
+    res.json(comments.filter(function (comment) { return comment.productID == req.params.id; }));
 });
 var server = app.listen(8000, 'localhost', function () {
     console.log('Server is listening on localhost:8000');
--- server/myServer/auction_server.ts
+++ server/myServer/auction_server.ts
@@ -18,13 +18,37 @@ export class Product {
 	}
 }
 
+export class Comment {
+
+	constructor(
+		public id:number,
+		public productID:number,
+		public timeStamp:string,
+		public user:string,
+		public rating:number,
+		public content:string
+
+		) {
+
+	}
+}
+
 const products:Array<Product> = [
 	  		new Product(1, "M99", 1.99, 3.5, "M99 is RF", ["RF"]),
 	  		new Product(2, "M1887", 4.99, 4.5, "M1887 is SG", ["SG"]),
 	  		new Product(3, "G41", 6.99, 2.5, "G41 is AR", ["AR"]),
 	  		new Product(4, "UMP45", 9.99, 5.0, "UMP45 is SMG", ["SMG"]),
 	  		new Product(5, "M950A", 2.99, 3.0, "M950A is HG", ["HG"]),
-	  		new Product(6, "MG4", 7.99, 1.5, "MG4 is MG", ["MG"]),
+	  		new Product(6, "MG4", 7.99, 1.5, "MG4 is MG", ["MG"])
+	  	];
+
+const comments:Array<Comment> = [
+	  		new Comment(1, 1, "2017-9-20 12:45", "sp1", 4, "Great"),
+	  		new Comment(2, 2, "2017-9-20 12:45", "sp1", 3, "Good"),
+	  		new Comment(3, 1, "2017-9-20 12:45", "sp1", 2.5, "Fair"),
+	  		new Comment(4, 3, "2017-9-20 12:45", "sp1", 5, "Excellent"),
+	  		new Comment(5, 2, "2017-9-20 12:45", "sp1", 1, "Bad"),
+	  		new Comment(6, 3, "2017-9-20 12:45", "sp1", 4, "Great")
 	  	];
 
 app.get('/', (req, res) => {
@@ -37,6 +61,10 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/product/:id', (req, res) => {
   res.json(products.find((product) => product.id == req.params.id));
+});
+
+app.get('/api/product/:id/comments', (req, res) => {
+  res.json(comments.filter((comment) => comment.productID == req.params.id));
 });
 
 const server = app.listen(8000, 'localhost', ()=> {
